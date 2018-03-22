3b7d111c988b9b7f425244edd10c2e265a3c28dc
Forever <lynk0613@hotmail.com>
2018-03-22 10:31:40 -0400

    env

---
+++ pages/customer/about/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+ 
+
+const About = props => (
+  <div>
+    <h1>About</h1>
+    <p>About!</p>
+     
+  </div>
+);
+
+export default About;
+
+ 
--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,36 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunkMiddleware from "redux-thunk";
+import thunk from "redux-thunk";
+import { routerMiddleware } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+import rootReducer from './../modules'
+
+
+ 
+let history
+
+if (typeof document !== 'undefined') {
+  const createBrowserHistory = require('history/createBrowserHistory').default
+
+  history = createBrowserHistory()
+}
+
+export default history
 
 const defaultState = {
   name: ""
 };
+
+const enhancers = []
+const middleware = [
+  thunk,
+  routerMiddleware(history)
+]
+
+const composedEnhancers = composeWithDevTools(
+  applyMiddleware(...middleware),
+  ...enhancers
+)
 
 export const actionTypes = {
   SET_NAME: "SET_NAME"
@@ -31,6 +57,8 @@ export const initStore = (initialState = defaultState) => {
   return createStore(
     reducer,
     initialState,
-    composeWithDevTools(applyMiddleware(thunkMiddleware))
+    composedEnhancers
+    //composeWithDevTools(applyMiddleware(thunkMiddleware)),
+
   );
 };
